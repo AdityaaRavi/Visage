2354dde98f3de461192edd260deb62c4c50cbdfb
Adityaa Ravi <adityaaravi6@gmail.com>
2023-01-06 08:30:15 -0800

    userStatsController successfully connected to the database.

--- server/userInfo/userStatsController.js
+++ server/userInfo/userStatsController.js
@@ -1,11 +1,22 @@
-function userStatsController(req, res) {
+function userStatsController(req, res, mysqlConnection) {
 
-    const userId = req.query.userId;
+  const userId = req.query.userId;
+
+  try{
+      mysqlConnection.query("use visage_app;", (err) => {if (err) throw err;});
+      mysqlConnection.query('SELECT num_connections, top_skill1, top_skill2, top_skill3, top_skill4 ' + 
+                            'FROM user_info WHERE userId=?;', [userId], (err, result) => {
+        if (err) throw err;
+        res.json({
+          numConnections: result[0].num_connections,
+          top4Skills: [result[0].top_skill1, result[0].top_skill2, result[0].top_skill3, result[0].top_skill4]
+        });
+      });
+  } catch (err) {
+    res.json(err);
+  }
     
-    res.json({
-      numConnections: 52,
-      top4Skills: ["React.js", "Tensorflow", "Java", "Leadership"],
-    });
+    
 }
 
 export default userStatsController;
