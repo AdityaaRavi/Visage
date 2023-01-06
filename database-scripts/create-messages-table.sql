addb0a71511e854eb3194fa3a7b6a2ec0bdb548c
Adityaa Ravi <adityaaravi6@gmail.com>
2023-01-06 15:42:46 -0800

    Added a unique "message_ID" field to identify each message.
    Previously, the sender_ID and recipient_ID togther were set as the primary key -- which won't be suitable as it would mean that each person will only ever send a single message to any other person.
    So I fixed this issue by creating a way to identify messages.

--- database-scripts/create-messages-table.sql
+++ database-scripts/create-messages-table.sql
@@ -4,6 +4,7 @@ CREATE TABLE `messages` (
 	`recipient_userID` INT unsigned NOT NULL,
 	`message_sent_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`message` VARCHAR(300) NOT NULL,
-	KEY `key2` (`recipient_userID`,`sender_userID`) USING BTREE,
-	PRIMARY KEY (`sender_userID`,`recipient_userID`)
+	`message_ID` INT unsigned NOT NULL,
+	KEY `key2` (`sender_userID`,`recipient_userID`) USING BTREE,
+	PRIMARY KEY (`message_ID`)
 );
