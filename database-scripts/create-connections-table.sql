fd93c63a9c03f5ddc22acf2fb6137254888a9164
Adityaa Ravi <adityaaravi6@gmail.com>
2023-01-11 13:43:43 -0800

    Added a field for a unique random number that can be used to identify each connection.

--- database-scripts/create-connections-table.sql
+++ database-scripts/create-connections-table.sql
@@ -1,8 +1,9 @@
 use `visage_app`;
 CREATE TABLE `connections` (
+	`connection_ID` INT unsigned NOT NULL,
 	`lower_userID` INT unsigned NOT NULL,
 	`higher_userID` INT unsigned NOT NULL,
 	`time_of_connection` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`lower_userID`,`higher_userID`),
-	KEY `key2` (`higher_userID`,`lower_userID`)
+	PRIMARY KEY (`connection_ID`),
+	KEY `key` (`higher_userID`,`lower_userID`)
 );
