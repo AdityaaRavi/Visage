c96a97efd460a11fc41b130709720579d1764f99
Adityaa Ravi <adityaaravi6@gmail.com>
2023-01-12 01:23:22 -0800

    sendMessage endpoint edited to allow it to create a connection when the user texts a suggested connection.

--- server/messaging/sendMessagesController.js
+++ server/messaging/sendMessagesController.js
@@ -7,21 +7,44 @@ const sendMessageController = (req, res, mysqlConnection) => {
     try{
       // Insert message into database
       mysqlConnection.query("use visage_app;", (err) => {if (err) throw err;});
-      
-      mysqlConnection.query('SELECT message_ID FROM messages;', (err, lis) => {
+
+      // check if the recepient is a connection
+      mysqlConnection.query('SELECT * FROM connections WHERE (lower_userID=? AND higher_userID=?) OR (lower_userID=? AND higher_userID=?);', [senderId, recepientId, recepientId, senderId], (err, result) => {
         if (err) throw err;
-        // Makes sure message_ID is unique. This does employ a full table scan though, which is not ideal at scale. This shouldn't cause any problems
-        //// in this application though.
-        let message_ID = Math.floor(Math.random() * 2147483647);
-        while(lis.includes(message_ID)) message_ID = Math.floor(Math.random() * 2147483647);
-      
-        mysqlConnection.query('INSERT INTO messages (sender_userID, recipient_userID, message, message_ID) ' +
-                              'VALUES (?, ?, ?, ?);', [senderId, recepientId, message, message_ID], (err, result) => {
-          if (err) throw err;
-          res.json({
-            message: `Message sent successfully! from ${senderId} to ${recepientId}`
+        // if the recepient is not a connection,
+        if (result.length == 0) {
+          // create a connection
+          mysqlConnection.query('SELECT connection_ID FROM connections;', (err, lis) => {
+            if (err) throw err;
+            // Makes sure connection_ID is unique. This does employ a full table scan though, which is not ideal at scale. This shouldn't cause any problems
+            //// in this application though.
+            let connectionId = Math.floor(Math.random() * 2147483647);
+            while(lis.includes(connectionId)) connectionId = Math.floor(Math.random() * 2147483647);
+
+            mysqlConnection.query('INSERT INTO connections (connection_ID, lower_userID, higher_userID) VALUES (?, ?, ?);', [connectionId, senderId > recepientId ? recepientId : senderId, senderId < recepientId ? recepientId : senderId], (err, result) => {
+              if (err) throw err;
+              // and change the suggested connection status to not pending
+              mysqlConnection.query('UPDATE suggested_connections SET pending=false, accepted=true WHERE (lower_userID=? AND higher_userID=?) OR (lower_userID=? AND higher_userID=?);', [senderId, recepientId, recepientId, senderId], (err, result) => {
+                if (err) throw err;
+              });
+            });
           });
-        });
+        }
+          mysqlConnection.query('SELECT message_ID FROM messages;', (err, lis) => {
+            if (err) throw err;
+            // Makes sure message_ID is unique. This does employ a full table scan though, which is not ideal at scale. This shouldn't cause any problems
+            //// in this application though.
+            let message_ID = Math.floor(Math.random() * 2147483647);
+            while(lis.includes(message_ID)) message_ID = Math.floor(Math.random() * 2147483647);
+          
+            mysqlConnection.query('INSERT INTO messages (sender_userID, recipient_userID, message, message_ID) ' +
+                                  'VALUES (?, ?, ?, ?);', [senderId, recepientId, message, message_ID], (err, result) => {
+              if (err) throw err;
+              res.json({
+                message: `Message sent successfully! from ${senderId} to ${recepientId}`
+              });
+            });
+          });
       });
 
     } catch (err) {
