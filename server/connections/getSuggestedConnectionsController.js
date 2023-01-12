32c8318ca3654d349c1112083a0ae324e1404216
Adityaa Ravi <adityaaravi6@gmail.com>
2023-01-12 00:50:32 -0800

    getSuggestedConnections api endpoint completed and integrated with the DB.

--- server/connections/getSuggestedConnectionsController.js
+++ server/connections/getSuggestedConnectionsController.js
@@ -1,14 +1,32 @@
-const getSuggestedConnectionsController = (req, res) => {
-    const userId = req.query.userId;
+const getSuggestedConnectionsController = (req, res, mysqlConnection) => {
+  const userId = req.query.userId;
+  try{
+    mysqlConnection.query("use visage_app;", (err) => {if (err) throw err;});
+    mysqlConnection.query('SELECT * FROM suggested_connections WHERE (lower_userID=? OR higher_userID=?) AND pending=true ORDER BY time_of_connection DESC;', [userId, userId], (err, result) => {
+      if (err) throw err;
+      let connections = result.map((connection) => connection.lower_userID == userId ? connection.higher_userID : connection.lower_userID);
+      let connectionsString = connections.join(' OR userId = ');
+
+      mysqlConnection.query(`SELECT * FROM user_info WHERE userId = ${connectionsString};`, (err, result) => {
+        console.log(result);
+        if (err) throw err;
+        let connectionsInfo = result.map((result) => {return {
+          id: result.userId,
+          name: result.name,
+          orgs: [result.org1, result.org2, result.org3, result.org4],
+          schools: [result.school1, result.school2, result.school3],
+          career: [result.career1, result.career2, result.career3],
+          fun: [result.fun1, result.fun2, result.fun3],
+          description: result.description
+        }});
+        res.json(connectionsInfo);
+      });
+    });
     
-    res.json([
-      {id: 1, name: 'John Doe', orgs: ['SacHacks'], schools: ['University of California, Davis'], career: ['Full Stack Development', 'Machine Learning'], fun: ['Biking', 'Badminton'], description: 'Open to chat and connect!'},
-      {id: 2, name: 'Jane Doe', orgs: ['Google DSC at UCD', 'UCD CS Tutoring Commitee'], schools: ['University of California, Davis'], career: ['Machine Learning', 'Hackathons'], fun: ['Travel', 'Badminton'], description: 'Open to discussing about Machine Learning and Hackathons!'},
-      {id: 3, name: 'John Smith', orgs: ['UCD CS Tutoring Commitee'], schools: ['University of California, Davis'], career: ['Full Stack Development', 'Hackathons'], fun: ['Biking', 'Travel'], description: 'Open to discussing about Full Stack Development and Hackathons!'},
-      {id: 4, name: 'John Doe', orgs: ['SacHacks'], schools: ['University of California, Davis'], career: ['Full Stack Development', 'Machine Learning'], fun: ['Biking', 'Badminton'], description: 'Open to chat and connect!'},
-      {id: 5, name: 'Jane Doe', orgs: ['Google DSC at UCD', 'UCD CS Tutoring Commitee'], schools: ['University of California, Davis'], career: ['Machine Learning', 'Hackathons'], fun: ['Travel', 'Badminton'], description: 'Open to discussing about Machine Learning and Hackathons!'},
-      {id: 6, name: 'John Smith', orgs: ['UCD CS Tutoring Commitee'], schools: ['University of California, Davis'], career: ['Full Stack Development', 'Hackathons'], fun: ['Biking', 'Travel'], description: 'Open to discussing about Full Stack Development and Hackathons!'},
-    ]);
+  } catch (err) {
+    res.json(err);
+    console.log(err);
+  }
 };
 
 export default getSuggestedConnectionsController;
