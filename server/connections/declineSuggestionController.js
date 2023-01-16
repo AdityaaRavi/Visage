36dba8bb00a4be1dc197cae95e715d01ce83591e
Adityaa Ravi <adityaaravi6@gmail.com>
2023-01-16 15:10:01 -0800

    DeclineSuggestion API endpoint completed and connected to the DB.

--- server/connections/declineSuggestionController.js
+++ server/connections/declineSuggestionController.js
@@ -1,15 +1,21 @@
-const declineSuggestionController = (req, res) => {
-    const userId = req.body.userId;
-    const otherPersonId = req.body.otherPersonId;
-  
-    try{
-      // Remove connection
+const declineSuggestionController = (req, res, mysqlConnection) => {
+  const userId = req.body.userId;
+  const otherPersonId = req.body.otherPersonId;
+    
+  try{
+    mysqlConnection.query("use visage_app;", (err) => {if (err) throw err;});
+    // Remove connection
+    mysqlConnection.query('UPDATE suggested_connections SET pending=false WHERE (lower_userID=? AND higher_userID=?) OR (lower_userID=? AND higher_userID=?);', [userId, otherPersonId, otherPersonId, userId], (err, result) => {
+      if (err) throw err;
       res.json({
-        message: `Connection suggestion declined successfully! from ${userId} to ${otherPersonId}`
+        message: `Connection suggestion declined successfully!`
       });
-    } catch (err) {
-      res.json(err);
-    }
+    });
+    
+  } catch (err) {
+    res.json(err);
+    console.log(err);
+  }
 };
 
 export default declineSuggestionController;
