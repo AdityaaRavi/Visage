0905cf95549b04ed4743c121a5eb3b01706f1faa
Adityaa Ravi <adityaaravi6@gmail.com>
2023-01-16 14:45:57 -0800

    removeConnection API endpoint completed and connected to DB.

--- server/connections/removeConnectionController.js
+++ server/connections/removeConnectionController.js
@@ -1,14 +1,20 @@
-const removeConnectionController = (req, res) => {
+const removeConnectionController = (req, res, mysqlConnection) => {
     const userId = req.body.userId;
     const otherPersonId = req.body.otherPersonId;
-  
+      
     try{
+      mysqlConnection.query("use visage_app;", (err) => {if (err) throw err;});
       // Remove connection
-      res.json({
-        message: `Connection removed successfully! from ${userId} to ${otherPersonId}`
+      mysqlConnection.query('DELETE FROM connections WHERE (lower_userID=? AND higher_userID=?) OR (lower_userID=? AND higher_userID=?);', [userId, otherPersonId, otherPersonId, userId], (err, result) => {
+        if (err) throw err;
+        res.json({
+          message: `Connection removed successfully!`
+        });
       });
+      
     } catch (err) {
       res.json(err);
+      console.log(err);
     }
 };
 
