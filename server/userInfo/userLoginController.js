83f2fea59b2cf4febc9e729723d6a3924a85154b
Adityaa Ravi <adityaaravi6@gmail.com>
2023-01-26 23:44:42 -0800

    userLogin controller completed. Upon a successful login, a sessionId unique to each login will be returned.
    All API endpoints must be edited to convert from the sessionId to the userId to make it compatible with this.
    
    It is better to not send the userId through the internet for security purposes.

---
+++ server/userInfo/userLoginController.js
@@ -0,0 +1,41 @@
+const userLoginController = (req, res, mysqlConnection) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    
+    try{
+      // Create user
+      mysqlConnection.query("use visage_app;", (err) => {if (err) throw err;});
+      mysqlConnection.query('SELECT userId FROM user_login WHERE email=? AND password=?;',(email, password) , (err, lis) => {
+        if(err) throw err;
+        if(lis.length == 0) res.json({error: "Email or password is incorrect!"});
+
+        // create a sessionId for the user and store it in the database
+        // return the sessionId to the user
+        mysqlConnection.query('SELECT sessionId FROM sessionsTable;', (err, lis2) => {
+          if (err) throw err;
+          // Makes sure sessionId is unique. This does employ a full table scan though, which is not ideal at scale. This shouldn't cause any problems
+          //// in this application though.
+          let sessionId = Math.floor(Math.random() * 2147483647);
+          while(lis2.includes(sessionId)) sessionId = Math.floor(Math.random() * 2147483647);
+          mysqlConnection.query('INSERT INTO sessionsTable (sessionId, userId) VALUES (?, ?);', [sessionId, lis[0].userId], (err, result) => {
+            if (err) throw err;
+            res.json({message: "Login successful!", sessionId: sessionId});
+          });
+        });
+      });
+    } catch (err) {
+      console.log(err);
+      res.json(err);
+    }
+};
+
+export default userLoginController;
+
+/*
+Example JSON body:
+{
+    "email": "test2@gmail.com",
+    "password": "pwd",
+} 
+*/
