5836df79e3cafd60e6e9f72410015240d14e87c3
Adityaa Ravi <adityaaravi6@gmail.com>
2023-01-27 00:37:24 -0800

    sample data entry creation scripts

---
+++ database-scripts/create-sample-sessions.sql
@@ -0,0 +1,6 @@
+INSERT INTO `sessionsTable` (sessionId, userId, isSession) values (6, 6, true);
+INSERT INTO `sessionsTable` (sessionId, userId, isSession) values (1, 1, true);
+INSERT INTO `sessionsTable` (sessionId, userId, isSession) values (2, 2, true);
+INSERT INTO `sessionsTable` (sessionId, userId, isSession) values (3, 3, true);
+INSERT INTO `sessionsTable` (sessionId, userId, isSession) values (7, 7, true);
+INSERT INTO `sessionsTable` (sessionId, userId, isSession) values (4, 7, false);
--- database-scripts/sample_suggested_connections.sql
+++ database-scripts/sample_suggested_connections.sql
@@ -3,4 +3,5 @@ insert into suggested_connections (connection_ID, lower_userID, higher_userID) v
 insert into suggested_connections (connection_ID, lower_userID, higher_userID) values (3, 3, 7);
 insert into suggested_connections (connection_ID, lower_userID, higher_userID) values (4, 1, 6);
 insert into suggested_connections (connection_ID, lower_userID, higher_userID) values (5, 2, 6);
+insert into suggested_connections (connection_ID, lower_userID, higher_userID) values (6, 2, 7);
 -- 1, 2, 3, 6, 7
