db26300b872b5f0c563c2ba28720d92b2b46892b
Adityaa Ravi <adityaaravi6@gmail.com>
2023-01-27 00:32:27 -0800

    Changed sessionId table to allow storing temp. Ids for non-logged users too.
    This is so that the userIds of users aren't freely sent to the frontend--greatly improving security.
    
    sessionId has been made the primary key as there might be multiple entries for each user.
    
    As there is no straight forward way to delete every tempId, added code to clean the table once per day.

--- database-scripts/create-sessionid-table.sql
+++ database-scripts/create-sessionid-table.sql
@@ -3,7 +3,16 @@ use `visage_app`;
 CREATE TABLE `sessionsTable` (
 	`userId` INT unsigned NOT NULL,
     `sessionId` INT unsigned NOT NULL,
+	`isSession` BOOLEAN NOT NULL,
     `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`userId`),
-    UNIQUE KEY `sessionId` (`sessionId`)
+	PRIMARY KEY (`sessionId`)
 );
+
+-- Clean up the sessions table once every day
+CREATE EVENT IF NOT EXISTS `session_cleaner_event`
+ON SCHEDULE
+  EVERY '1 4' DAY_HOUR
+  COMMENT 'Clean up tempIds and sessions older than 5 hours at 4:00 AM daily!'
+  DO
+    DELETE FROM `sessionsTable`
+    WHERE TIMESTAMPDIFF(HOURS, NOW(),sessionsTable.timestamp)>5;
