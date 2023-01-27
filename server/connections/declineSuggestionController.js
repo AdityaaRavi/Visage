2adeb50275fd37d4780ea616a9a11d3d77927fdd
Adityaa Ravi <adityaaravi6@gmail.com>
2023-01-27 00:48:14 -0800

    commit made to switch branches

--- server/connections/declineSuggestionController.js
+++ server/connections/declineSuggestionController.js
@@ -1,17 +1,22 @@
 const declineSuggestionController = (req, res, mysqlConnection) => {
-  const userId = req.body.userId;
-  const otherPersonId = req.body.otherPersonId;
+  const sessionId = req.body.userId;
+  const otherPersonTempId = req.body.otherPersonId;
     
   try{
     mysqlConnection.query("use visage_app;", (err) => {if (err) throw err;});
     // Remove connection
-    mysqlConnection.query('UPDATE suggested_connections SET pending=false WHERE (lower_userID=? AND higher_userID=?) OR (lower_userID=? AND higher_userID=?);', [userId, otherPersonId, otherPersonId, userId], (err, result) => {
-      if (err) throw err;
-      res.json({
-        message: `Connection suggestion declined successfully!`
+    // go from sessionId to userId
+    mysqlConnection.query('SELECT userID FROM users WHERE sessionID=?;', [sessionId], (err, result) => {
+      mysqlConnection.query('SELECT userID FROM users WHERE sessionID=?;', [otherPersonTempId], (err, result2) => {
+
+        mysqlConnection.query('UPDATE suggested_connections SET pending=false WHERE (lower_userID=? AND higher_userID=?) OR (lower_userID=? AND higher_userID=?);', [result[0].userID, result2[0].userID, result2[0].userID, result[0].userID,], (err, result) => {
+          if (err) throw err;
+          res.json({
+            message: `Connection suggestion declined successfully!`
+          });
+        });
       });
     });
-    
   } catch (err) {
     res.json(err);
     console.log(err);
--- server/connections/getExistingConnectionsController.js
+++ server/connections/getExistingConnectionsController.js
@@ -1,28 +1,36 @@
 const getExistingConnectionsController = (req, res, mysqlConnection) => {
 
-  const userId = req.query.userId;
+  const sessionId = req.query.userId;
+
+  // 
+
   try{
     mysqlConnection.query("use visage_app;", (err) => {if (err) throw err;});
-    mysqlConnection.query('SELECT * FROM connections WHERE lower_userID=? OR higher_userID=? ORDER BY time_of_connection DESC;', [userId, userId], (err, result) => {
+    // go from session id to user id
+    mysqlConnection.query('SELECT userId FROM sessions WHERE sessionId=?;', [sessionId], (err, result) => {
       if (err) throw err;
-      let connections = result.map((connection) => connection.lower_userID == userId ? connection.higher_userID : connection.lower_userID);
-      let connectionsString = connections.join(' OR userId = ');
-
-      mysqlConnection.query(`SELECT * FROM user_info WHERE userId = ${connectionsString};`, (err, result) => {
+      let userId = result[0].userId;
+      mysqlConnection.query('SELECT * FROM connections WHERE lower_userID=? OR higher_userID=? ORDER BY time_of_connection DESC;', [userId, userId], (err, result) => {
         if (err) throw err;
-        let connectionsInfo = result.map((result) => {return {
-          id: result.userId,
-          name: result.name,
-          orgs: [result.org1, result.org2, result.org3, result.org4],
-          schools: [result.school1, result.school2, result.school3],
-          career: [result.career1, result.career2, result.career3],
-          fun: [result.fun1, result.fun2, result.fun3],
-          description: result.description
-        }});
-        res.json(connectionsInfo);
+        let connections = result.map((connection) => connection.lower_userID == userId ? connection.higher_userID : connection.lower_userID);
+        let connectionsString = connections.join(' OR userId = ');
+
+        mysqlConnection.query(`SELECT * FROM user_info WHERE userId = ${connectionsString};`, (err, result) => {
+          if (err) throw err;
+          let connectionsInfo = result.map((result) => {return {
+            id: result.userId,
+            name: result.name,
+            orgs: [result.org1, result.org2, result.org3, result.org4],
+            schools: [result.school1, result.school2, result.school3],
+            career: [result.career1, result.career2, result.career3],
+            fun: [result.fun1, result.fun2, result.fun3],
+            description: result.description
+          }});
+          createAndSendTempId(connectionsInfo);
+          res.json(connectionsInfo);
+        });
       });
     });
-    
   } catch (err) {
     res.json(err);
     console.log(err);
