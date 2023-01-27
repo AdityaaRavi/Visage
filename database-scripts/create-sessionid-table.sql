e53580d12bdea109282d99bde34439ab499e7f30
Adityaa Ravi <adityaaravi6@gmail.com>
2023-01-27 00:35:51 -0800

    commenting out the auto-clean up code for the session table db for now

--- database-scripts/create-sessionid-table.sql
+++ database-scripts/create-sessionid-table.sql
@@ -9,10 +9,10 @@ CREATE TABLE `sessionsTable` (
 );
 
 -- Clean up the sessions table once every day
-CREATE EVENT IF NOT EXISTS `session_cleaner_event`
-ON SCHEDULE
-  EVERY '1 4' DAY_HOUR
-  COMMENT 'Clean up tempIds and sessions older than 5 hours at 4:00 AM daily!'
-  DO
-    DELETE FROM `sessionsTable`
-    WHERE TIMESTAMPDIFF(HOURS, NOW(),sessionsTable.timestamp)>5;
+-- CREATE EVENT IF NOT EXISTS `session_cleaner_event`
+-- ON SCHEDULE
+--   EVERY '1 4' DAY_HOUR
+--   COMMENT 'Clean up tempIds and sessions older than 5 hours at 4:00 AM daily!'
+--   DO
+--     DELETE FROM `sessionsTable`
+--     WHERE TIMESTAMPDIFF(HOURS, NOW(),sessionsTable.timestamp)>5;
