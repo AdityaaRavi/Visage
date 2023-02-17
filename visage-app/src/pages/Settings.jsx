2342ddf09da3aec925ba00a8b66363d38a574a53
Adityaa Ravi <adityaaravi6@gmail.com>
2023-02-16 20:39:15 -0800

    Log out implemented

--- visage-app/src/pages/Settings.jsx
+++ visage-app/src/pages/Settings.jsx
@@ -1,11 +1,23 @@
 import './page-styles.css';
+// import { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
+// import axios from 'axios';
+// import { useSelector, useDispatch } from 'react-redux'
+// import { login, logOut } from '../redux-slices/userIdSlice'
 
 function Settings(){
+
+  const logOut = () => {
+    localStorage.removeItem('userId');
+  }
   
   return (
     <div>
         <h1> Settings </h1>
-        
+        {/* Button to log out */}
+        <Button variant='primary' className='logoutButton' onClick={logOut} href='/'>
+            Log Out
+        </Button>
     </div>
    );
 }
