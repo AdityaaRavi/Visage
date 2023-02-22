d22bf4851ab265c1267002b0d84e7146ee6cc575
Adityaa Ravi <adityaaravi6@gmail.com>
2023-02-21 23:09:29 -0800

    All connection related frontend components updated to deal with sessionId

--- visage-app/src/pages/ConnectPage.jsx
+++ visage-app/src/pages/ConnectPage.jsx
@@ -12,8 +12,17 @@ function ConnectPage(props){
 
   const onDeclineConnectionButtonClick = (e) => {
     /* AJAX call to remove connection */
-    axios.post('/declineSuggestion', {userId: props.id, otherPersonId: otherPersonId})
+    axios.post('/declineSuggestion', {userId: props.id, otherPersonId: otherPersonId, session: props.session})
          .then((response) => {
+            // if not logged in, redirect to login page
+            if (response.data === 'Not logged in') {
+              console.log('Not logged in');
+              // clear user Id and session Id from local storage and redirect to login page
+              localStorage.removeItem('userId');
+              localStorage.removeItem('sessionId');
+              window.location.href = '/';
+              return;
+            }
             console.log(response.data);
             setOtherPerson(-1);
           });         
@@ -23,15 +32,15 @@ function ConnectPage(props){
     <div id='ConnectionsPage'>
       <div class='personPicker'>
         <h1>Suggested Connections</h1>
-        <PersonPicker className='personPickerComponent' id={props.id} picker={setOtherPerson} otherPersonId={otherPersonId} getNew={true}/>
+        <PersonPicker className='personPickerComponent' id={props.id} picker={setOtherPerson} otherPersonId={otherPersonId} getNew={true} session={props.session}/>
       </div>
       <div class='VerticalDivider'></div>
         <div class='messageHolderLvl2'>
-          <Profile id={otherPersonId != -1 ? otherPersonId : props.id} inConnectionPage={true}/>
+          <Profile id={otherPersonId != -1 ? otherPersonId : props.id} inConnectionPage={true} session={props.session}/>
           {/* id is to identify the current user, person 
           is to identify the person at the other end of the conversation */}
           <div>Start a conversation:</div>
-          <MessagesInputBox id={props.id} otherPersonId={otherPersonId} />
+          <MessagesInputBox id={props.id} otherPersonId={otherPersonId} session={props.session}/>
           <div>
             <Button variant='primary' 
             className='declineConnectionButton'
