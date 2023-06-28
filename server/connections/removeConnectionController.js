b836c753fc205118f0c72c84c12430b7e3cbc668
Adityaa Ravi <adityaaravi6@gmail.com>
2023-06-27 19:51:27 -0500

    Updating the `num_connections` field for each user when a connection is removed.

--- server/connections/removeConnectionController.js
+++ server/connections/removeConnectionController.js
@@ -7,6 +7,8 @@ const removeConnectionController = (req, res, mysqlConnection) => {
       // Remove connection
       mysqlConnection.query('DELETE FROM connections WHERE (lower_userID=? AND higher_userID=?) OR (lower_userID=? AND higher_userID=?);', [userId, otherPersonId, otherPersonId, userId], (err, result) => {
         if (err) throw err;
+        // update the number of connections field for both the users
+        mysqlConnection.query('UPDATE user_info SET num_connections=num_connections-1 WHERE userID=? OR userID=?;', [userId, otherPersonId],  (err, result) => {if (err) throw err;});
         res.json({
           message: `Connection removed successfully!`
         });
